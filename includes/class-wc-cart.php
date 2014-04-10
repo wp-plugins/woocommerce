d1aba173bf39ca5da99aa41a59265de6d778bb6f
mikejolley <mikejolley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-10 14:23:47 +0000

    Committing 2.1.7
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce/trunk@890626 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-wc-cart.php
+++ includes/class-wc-cart.php
@@ -1113,7 +1113,7 @@ class WC_Cart {
 						$taxes             = $this->tax->calc_tax( $line_price, $base_tax_rates, true, true );
 
 						// Now we have a new item price (excluding TAX)
-						$line_subtotal     = $line_price - array_sum( $taxes );
+						$line_subtotal     = round( $line_price - array_sum( $taxes ), WC_ROUNDING_PRECISION );
 
 						// Now add modifed taxes
 						$taxes             = $this->tax->calc_tax( $line_subtotal, $item_tax_rates );
@@ -1353,6 +1353,22 @@ class WC_Cart {
 			}
 
 			return apply_filters( 'woocommerce_cart_needs_shipping', $needs_shipping );
+		}
+
+		/**
+		 * Should the shipping address form be shown
+		 * 
+		 * @return bool
+		 */
+		function needs_shipping_address() {
+
+			$needs_shipping_address = false;
+
+			if ( WC()->cart->needs_shipping() === true && ! WC()->cart->ship_to_billing_address_only() ) {
+				$needs_shipping_address = true;
+			}
+
+			return apply_filters( 'woocommerce_cart_needs_shipping_address', $needs_shipping_address );
 		}
 
 		/**
