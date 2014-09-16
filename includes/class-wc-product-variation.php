5b5f3a63aa66f93775a6b6216ef36f9ebeb298f0
claudiosanches <claudiosanches@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-16 13:00:35 +0000

    version 2.2.3
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce/trunk@991011 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/class-wc-product-variation.php
+++ includes/class-wc-product-variation.php
@@ -59,6 +59,7 @@ class WC_Product_Variation extends WC_Product {
 		'sku'        => '',
 		'weight'     => '',
 		'length'     => '',
+		'width'      => '',
 		'height'     => ''
 	);
 
@@ -137,6 +138,9 @@ class WC_Product_Variation extends WC_Product {
 
 		} elseif ( 'variation_data' === $key ) {
 			$all_meta = get_post_meta( $this->variation_id );
+
+			// The variation data array
+			$this->variation_data = array();
 
 			// Get the variation attributes from meta
 			foreach ( $all_meta as $name => $value ) {
